6ce6b2a70b34d92862a3e3ab439c870a32fa3004
Katherine Shatskikh <eshatskikh@gmail.com>
2021-10-10 19:40:13 -0400

    Updated events with Eerie Update.

--- app.R
+++ app.R
@@ -14,8 +14,28 @@ duration_festival <- 50
 
 # TH Fright night
 prizes <- data.frame(relic = numeric(15), bunnies = numeric(15))
-prizes$relic <- c(50, 120, 200, 120, 50, 150, 250, 250, 150, 360, 380, 360, 520, 520, 750)
-prizes$bunnies <- c(0, 15, 30, 15, 0,35, 70 ,70, 35, 75, 150, 75, 140, 140, 300)
+#Rerun:
+prizes$relic <- c(60, 120, 150, 120, 60, 
+                  170, 250, 250, 170, 
+                  340, 530, 340, 
+                  560, 560, 
+                  1000)
+prizes$bunnies <- c(0, 15, 30, 15, 0, 
+                    10, 65, 65, 10, 
+                    85, 130, 85, 
+                    160, 160, 
+                    320)
+# #New:
+# prizes$relic <- c(50, 120, 200, 120, 50, 
+#                   150, 250, 250, 150, 
+#                   360, 380, 360, 
+#                   520, 520, 
+#                   750)
+# prizes$bunnies <- c(0, 15, 30, 15, 0,
+#                     35, 70 ,70, 35, 
+#                     75, 150, 75, 
+#                     140, 140, 
+#                     300)
 prizes$id <- seq(1,15)
 
 
@@ -28,13 +48,18 @@ prizes$id <- seq(1,15)
 # # STS: Treatsylvania
  price_per_sweet <- 300
  price_per_royal <- 7
- max_sweets <- 45
+ max_sweets <- 58
  max_royal <- 24
  
  
 # Season Festival 
 tier <- seq(1,60)
-pink_tickets <- c(0,200,240,270,300,330,360,390,420,450,480,500,510,520,530,540,560,570,580,590,610,620,630,640,660,670,680,700,710,730,740,750,770,780,800,810,830, 840,860,870,890,900,920,930,940,950, 950,960, 970,970,970,980,980,980,990,990,990, 1000,1000,1000)
+pink_tickets <- c(0,200,240,270,300,330,360,390,420,450,
+                  480,500,510,520,530,540,560,570,580,590,
+                  610,620,630,640,660,670,680,700,710,730,
+                  740,750,770,780,800,810,830, 840,860,870,
+                  890,900,920,930,940,950,950,960,970,970,
+                  970,980,980,980,990,990,990,1000,1000,1000)
 
 seasontasks<-c(3650, 0, 0,
                2000, 0, 0,
@@ -65,16 +90,18 @@ ui <- fluidPage(
     
     # Application title
     img(src='Yani.png',height=50, align ="right"),
+    img(src='SaveTheSimsMobile.jpeg',height=50, align ="right"),
+    #a(imageOutput("SaveTheSimsMobile.jpeg"), href="https://www.google.com"),
     titlePanel("The Sims Mobile: Event Calculator"),
     helpText("This is an interactive app to check your progress in the The Sims Mobile's events. "),
+    tagList("If you feel the game became boring and repetive with all the reruns and no new content, please support", a("#SaveTheSimsMobile", href="https://twitter.com/hashtag/SaveTheSimsMobile?src=hashtag_click"), "."),
     
     
     tabsetPanel(
-       
         #New Panel: 
-        tabPanel("Treasure Hunt: Beatnik ", fluid = TRUE , 
-                 helpText("During this Treasure Hunt event you need to collect 4230 relics and 1150 bunnies over 11 days. 
-                  Therefore you need to collect on average  384.55 relics a day and 104.55  bunnies a day to get the grand prize.
+        tabPanel("Treasure Hunt: Fright Night (Rerun)", fluid = TRUE , 
+                 helpText("During this Treasure Hunt event you need to collect 4680 relics and 1150 bunnies over 11 days. 
+                  Therefore you need to collect on average  425.45 relics a day and 104.55  bunnies a day to get the grand prize.
                   Fill out the information in the grey box to compute the daily average number of tokens.  
                   "),
                  
@@ -84,20 +111,20 @@ ui <- fluidPage(
                      
                      sidebarPanel(
                          helpText("Select the prizes you own/unlocked:"),
-                         checkboxInput("code1", "Row 1: Menorah of Memories"), 
-                         checkboxInput("code2", "Row 1: Cropped to Go Pants"), 
-                         checkboxInput("code3","Row 1: Too Hot to Handle Camisole"),
-                         checkboxInput("code4","Row 1: True Auteur's Chair"),
-                         checkboxInput("code5","Row 1: Hanging Out Plant Pot"), 
-                         checkboxInput("code6","Row 2: Bass Drop Rug"), 
-                         checkboxInput("code7","Row 2: Long Cat Knee High Tights"), 
-                         checkboxInput("code8","Row 2: Yes Henny! Henley PJ Top"),
-                         checkboxInput("code9","Row 2: Totally Triangles Window"), 
-                         checkboxInput("code10","Row 3: Tufted Tucker-me-not Bed"), 
-                         checkboxInput("code11","Row 3: Sweatproof Summer Hawk Hair"), 
-                         checkboxInput("code12","Row 3: Abstract Comfort Couch"),
-                         checkboxInput("code13","Row 4: Sumptuous Spring Gown"), 
-                         checkboxInput("code14","Row 4: Too Pretty to be Dirty Shower"),
+                         checkboxInput("code1", "Row 1: Floral Wall Sconce"), 
+                         checkboxInput("code2", "Row 1: Twisted Woods Mirror"), 
+                         checkboxInput("code3","Row 1: Lunar Phase Painting"),
+                         checkboxInput("code4","Row 1: High Backed Dining Chair"),
+                         checkboxInput("code5","Row 1: Carved Curio Table"), 
+                         checkboxInput("code6","Row 2: Wise Wizard's Bookcase"), 
+                         checkboxInput("code7","Row 2: Carved Curio Drawers"), 
+                         checkboxInput("code8","Row 2: Art Nouveau Toilet"),
+                         checkboxInput("code9","Row 2: Future Sight Crystal Ball"), 
+                         checkboxInput("code10","Row 3: Apothacary's Cabinet"), 
+                         checkboxInput("code11","Row 3: Midweek Madness Dress"), 
+                         checkboxInput("code12","Row 3: Alchemist's Display"),
+                         checkboxInput("code13","Row 4: Pretty As A Peacock Basin"), 
+                         checkboxInput("code14","Row 4: Carved Curio Wardrobe"),
                          
                          #checkboxInput("code15","Grand Prize"),
                          br(),
@@ -123,14 +150,14 @@ ui <- fluidPage(
                                                       label = "Relics:", 
                                                       value = 0,
                                                       min = 0,
-                                                      max = 4680,
+                                                      max = sum(prizes$relic),
                                                       width = "70px") ),
                                   
                                   column(5, offset=1, numericInput(inputId="bunnies", 
                                                                    label= "Bunnies:", 
                                                                    value=0, 
                                                                    min=0,
-                                                                   max=1150,
+                                                                   max= sum(prizes$bunnies),
                                                                    width = "70px")))#,
                          #helpText("Note: the maximum value for relics and bunnies is 4680 and 1150 respectively.")
                          
@@ -149,7 +176,7 @@ ui <- fluidPage(
                          textOutput("walkie_talkies_count"),
                          br(),
                          helpText("Below you can see the pictures of the prizes as well as the pyramid structure of the event."),
-                         img(src='TH_Beatnik.png', width=400), 
+                         img(src='bbpp3r51c5s71.jpg', width=400), 
                          br(),
                          #tagList("Infographic Source:", a("SimmerdownMal's Twitter", href="https://twitter.com/simmerdownMAL/"),"."),
                          tagList("Inforgraphic Source:", a("Salixcat's website", href="https://www.salixcat.com"),"."),
@@ -168,11 +195,12 @@ ui <- fluidPage(
         
         
         , 
+       
         
         # Another tab: 
-        tabPanel("Sweet Treat Showdown: Catwalk Fabulous (Rerun)", fluid = TRUE, 
-                 helpText("During this Sweet Treat Showdown event you need to collect 13500 sweet treats (45 boxes) and 168 royal cookies (24 boxes) over 11 days. 
-                  Therefore you need to collect on average  1227.27 sweet treats a day and 15.27  royal cookies a day to get the grand prize.
+        tabPanel("Sweet Treat Showdown: Treatsylvania Returns (Rerun)", fluid = TRUE, 
+                 helpText("During this Sweet Treat Showdown event you need to collect 17400 sweet treats (58 boxes) and 168 royal cookies (24 boxes) over 11 days. 
+                  Therefore you need to collect on average  1581.82  sweet treats a day and 15.27  royal cookies a day to get the grand prize.
                   Fill out the information in the grey box to compute the daily average number of tokens based on your progress in the event.  
                   "),
                  sidebarLayout(
@@ -242,10 +270,10 @@ ui <- fluidPage(
                          br(),
                          helpText("Here is more information about the event:"),
                          #helpText("Below you can see the pictures of the prizes as well as the pyramid structure of the event."),
-                         img(src='STS_CatwalkFabulous.png', height=600), 
+                         img(src='5xutir03c5s71.jpg', height=600), 
                          br(),
-                         #tagList("Infographic Source:", a("SimmerdownMal's Twitter", href="https://twitter.com/simmerdownMAL/"),"."),
-                         tagList("Infographic Source:", a("Salixcat's website", href="https://www.salixcat.com"),"."),
+                         tagList("Infographic Source:", a("SimmerdownMal's Twitter", href="https://twitter.com/simmerdownMAL/"),"."),
+                         #tagList("Infographic Source:", a("Salixcat's website", href="https://www.salixcat.com"),"."),
                          br(), 
                          #helpText("This app was created by Yani. If you have any questions or comments, you can contact me on Twitter: "),
                          tagList("This app was created by Yani. If you have any questions or comments, you can find me on ", a("Twitter", href="https://twitter.com/yani_tsm"),", ", a("Reddit", href="https://www.reddit.com/r/SimsMobile/"), "or most of the Sims Mobile Discord servers. Any feedback is appreciated.")
@@ -253,6 +281,8 @@ ui <- fluidPage(
                  )
         )# end of tabPanel
         ,
+        
+        
         
         
         # #New Panel: 
@@ -318,7 +348,7 @@ ui <- fluidPage(
                          br(),
                          #helpText("Remember that you receive 550 prize tickets for completing daily tasks and at least 500 for completing each seasonal task (you get 3 seasonal tasks every 3 days)
                          #         Additional prize tickets can be received by completing the season tasks which are being added every 3 days."),
-                         img(src='y2x7trra2zg71.jpg', height=600), 
+                         img(src='osh6fe1zb5s71.jpg', width = 400), 
                          br(),
                          #tagList("Infographic Source:", a("SimmerdownMal's Twitter", href="https://twitter.com/simmerdownMAL/"),"."),
                          tagList("Infographic Source:", a("Salixcat's website", href="https://www.salixcat.com"),"."),
@@ -372,13 +402,23 @@ server <- function(input, output) {
     })
     
     output$tokens_needed_sts <- renderText({
-        total_relics <- input$sweet_boxes*price_per_sweet - input$sweet_treats
-        total_bunnies <- input$royal_boxes*price_per_royal - input$royal_cookies
-        total_relics <- ifelse( total_relics < 0, "ERROR",  total_relics)
-        total_bunnies  <- ifelse( total_bunnies < 0, "ERROR",  total_bunnies )
+        if (input$sweet_boxes == 0 & input$sweet_treats > 0) {
+            total_relics = 0
+            perc_relics = 0
+        }  else {
+            total_relics <- input$sweet_boxes*price_per_sweet - input$sweet_treats
+            total_relics <- ifelse( total_relics < 0, "ERROR",  total_relics)
+            perc_relics <- round(total_relics/(max_sweets*price_per_sweet)*100, 2)
+        }
         
-        perc_relics <- round(total_relics/(max_sweets*price_per_sweet)*100, 2)
-        perc_bunnies <- round(total_bunnies/(max_royal*price_per_royal)*100, 2)
+        if (input$royal_boxes == 0 & input$royal_cookies > 0) {
+            total_bunnies = 0
+            perc_bunnies = 0
+        } else {
+            total_bunnies <- input$royal_boxes*price_per_royal - input$royal_cookies
+            total_bunnies  <- ifelse( total_bunnies < 0, "ERROR",  total_bunnies )
+            perc_bunnies <- round(total_bunnies/(max_royal*price_per_royal)*100, 2)
+        }
         paste("You still need to collect ", total_relics, " sweet treats (",perc_relics,"% of all sweet treats) and ", total_bunnies , " royal cookies ( ",perc_bunnies ,"% of all royal cookies).")
     })
     
@@ -386,11 +426,19 @@ server <- function(input, output) {
         
         time_left <- input$day_left_sts + input$hour_left_sts/24
         
-        relics_a_day <- round((input$sweet_boxes*price_per_sweet - input$sweet_treats)/ time_left, 2)
-        bunnies_a_day <- round((input$royal_boxes*price_per_royal - input$royal_cookies)/ time_left, 2)
+        if (input$sweet_boxes == 0 & input$sweet_treats > 0) {
+            relics_a_day = 0
+        } else {
+            relics_a_day <- round((input$sweet_boxes*price_per_sweet - input$sweet_treats)/ time_left, 2)
+            relics_a_day <- ifelse(relics_a_day < 0, "ERROR", relics_a_day)
+        }
+        if (input$royal_boxes == 0 & input$royal_cookies > 0) {
+            bunnies_a_day = 0
+        } else {
+            bunnies_a_day <- round((input$royal_boxes*price_per_royal - input$royal_cookies)/ time_left, 2)
+            bunnies_a_day   <- ifelse(bunnies_a_day < 0, "ERROR", bunnies_a_day)
+        }
         
-        relics_a_day <- ifelse(relics_a_day < 0, "ERROR", relics_a_day)
-        bunnies_a_day   <- ifelse(bunnies_a_day < 0, "ERROR", bunnies_a_day)
         paste("To get the Grand prize you need to collect on average ", relics_a_day, " sweet treats a day and ", bunnies_a_day, " royal cookies a day." )
     })
     
@@ -399,12 +447,15 @@ server <- function(input, output) {
         codes <- !(c(input$code1,input$code2, input$code3, input$code4, input$code5, 
                      input$code6,input$code7, input$code8, input$code9, input$code10, 
                      input$code11,input$code12, input$code13, input$code14, FALSE))
-        total_relics <- sum(codes*prizes$relic) - input$relics
-        total_bunnies <- sum(codes*prizes$bunnies) - input$bunnies
-        total_relics <- ifelse( total_relics < 0, "ERROR",  total_relics)
-        total_bunnies  <- ifelse( total_bunnies < 0, "ERROR",  total_bunnies )
         
+        
+        total_relics <- sum(codes*prizes$relic) - input$relics
+        total_relics <- ifelse( total_relics < 0, "ERROR",  total_relics)
         perc_relics <- round(total_relics/sum(prizes$relic)*100, 2)
+        
+        
+        total_bunnies <- sum(codes*prizes$bunnies) - input$bunnies
+        total_bunnies  <- ifelse( total_bunnies < 0, "ERROR",  total_bunnies )
         perc_bunnies <- round(total_bunnies/sum(prizes$bunnies)*100, 2)
         
         #paste("You still need to collect ", total_relics, " relics and ", total_bunnies , " bunnies.")
@@ -420,10 +471,11 @@ server <- function(input, output) {
         time_left <- input$day_left_th + input$hour_left_th/24
         
         relics_a_day <- round((sum(codes * prizes$relic)- input$relics)/ time_left, 2)
-        bunnies_a_day <- round((sum(codes * prizes$bunnies)- input$bunnies)/ time_left, 2)
-        
         relics_a_day <- ifelse(relics_a_day < 0, "ERROR", relics_a_day)
+        
+        bunnies_a_day <- round((sum(codes * prizes$bunnies)- input$bunnies)/ time_left, 2)
         bunnies_a_day   <- ifelse(bunnies_a_day < 0, "ERROR", bunnies_a_day)
+        
         paste("To get the Grand prize you need to collect on average ", relics_a_day, " relics a day and ", bunnies_a_day, " bunnies a day." )
     })
     
@@ -455,14 +507,14 @@ server <- function(input, output) {
         hours_left <- input$day_left_th*24 + input$hour_left_th
         
         number_left <- floor(hours_left/5)
-        paste("You should expect additional ", number_left, "Walkie Talkies in the next ", input$day_left_th, "days and ", input$hour_left_th, " hours. Note: you can store up to 5 Walkie Talkies (or 10 Walkie Talkies if you purchase Treasure Hunt Explorer's Kit). You can also pay 30 simcash for additional Walkie Talkies. You will also get up to 10 Walkie Talkies upon buying the Treasure Hunt Explorer's Kit.")
+        paste("You should expect approximately ", number_left, "Walkie Talkies in the next ", input$day_left_th, "days and ", input$hour_left_th, " hours. Note: you can store up to 5 Walkie Talkies (or 10 Walkie Talkies if you purchase Treasure Hunt Explorer's Kit). You can also pay simcash for additional Walkie Talkies. You will also get up to 10 Walkie Talkies upon buying the Treasure Hunt Explorer's Kit.")
     })
     
     output$challenges_count <- renderText({
         hours_left <- input$day_left_sts*24 + input$hour_left_sts
         
         number_left <- floor(hours_left/8)
-        paste("You should expect additional ", number_left, "challenges in the next ", input$day_left_th, "days and ", input$hour_left_th, " hours. Note: you can store up to 3 challenges (or 6 challenges if you purchase Pro Ticket). You can also pay 30 simcash for additional challenge. You will also get up to 6 challenges upon buying the Pro Ticket.")
+        paste("You should expect approximately ", number_left, "challenges in the next ", input$day_left_sts, "days and ", input$hour_left_sts, " hours. Note: you can store up to 3 challenges (or 6 challenges if you purchase Pro Ticket). You can also pay simcash for additional challenge. You will also get up to 6 challenges upon buying the Pro Ticket.")
     })
 }
 
